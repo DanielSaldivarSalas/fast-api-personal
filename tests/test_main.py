717a97c4a41ffd83ec89c3f8331ce97737c16638
Daniel Saldivar <daniel.saldivar.ds@gmail.com>
2021-04-15 16:10:35 -0400

    100% test coverage

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,18 @@ from fastapi.testclient import TestClient
 client = TestClient(app)
 
 def test_read_root():
+    #when
     response = client.get("/")
+
+    #then
     assert response.status_code == 200
     assert response.json() == {"msg": "Hello World"}
+
+def test_read_item():
+    #when
+    response = client.get("/items/87")
+
+    #then
+    assert response.status_code == 200
+    assert response.json() == {"item_id": 87,
+                                "q": None,} 
